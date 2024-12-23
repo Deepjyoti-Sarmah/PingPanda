3386665367b2782291d2f3b7ec3a7e96ed861c8d
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-24 00:04:48 +0530

    setting up landing page with moke discord

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -2,16 +2,16 @@ import { cn } from '@/utils'
 import { ReactNode } from 'react'
 
 interface MaxWidthWrapperProps {
-  classname?: string
+  className?: string
   children: ReactNode
 }
 
 export const MaxWidthWrapper = ({
-  classname,
+  className,
   children }: MaxWidthWrapperProps) => {
   return (
     <div
-      className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md: px-20", classname)}>
+      className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
       {children}
     </div>
   )
