e645db6fd8b9331d763432f9beb45f39e38395a8
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-25 00:03:47 +0530

    adding auth route using tanstak-query, hono and trpc

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -5,6 +5,9 @@ import { Redis } from "@upstash/redis/cloudflare"
 import { env } from "hono/adapter"
 import { cacheExtension } from "./__internals/db/cache-extension"
 import { j } from "./__internals/j"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
 ///**
 // * Middleware for providing a built-in cache with your Prisma database.
@@ -34,6 +37,36 @@ import { j } from "./__internals/j"
 //  return await next({ db })
 //})
 //
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  const authHeader = c.req.header("Authorization")
+
+  if (authHeader) {
+    const apiKey = authHeader.split(" ")[1]
+
+    const user = await db.user.findUnique({
+      where: { apiKey },
+    })
+
+    if (user) return next({ user })
+  }
+
+  const auth = await currentUser()
+
+  if (!auth) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  return next({ user })
+})
+//
 ///**
 // * Public (unauthenticated) procedures
 // *
@@ -41,3 +74,4 @@ import { j } from "./__internals/j"
 // */
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
+export const privateProcedure = publicProcedure.use(authMiddleware)
