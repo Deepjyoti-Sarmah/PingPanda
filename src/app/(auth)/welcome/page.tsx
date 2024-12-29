4b85def41f714ad93e1d8a48e1fcea9decef7f50
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-29 21:47:21 +0530

    adding timeout

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
     },
     queryKey: ["get-database-sync-status"],
     refetchInterval: (query) => {
-      return query.state.data?.isSynced ? false : 1000
+      return query.state.data?.isSynced ? false : 3000
     },
   })
 
